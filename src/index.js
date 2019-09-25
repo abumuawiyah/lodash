57231fd0c8da878891aaa2bd71afac37e121e72d
Azizi Yazit <ibnuyazit@gmail.com>
2019-09-25 17:39:52 +0800

    change label

--- src/index.js
+++ src/index.js
@@ -32,7 +32,7 @@ console.log("native forEach", _.each([1, 2, 3, 5], d => console.log(d)));
 console.log("lodash", _.each([1, 2, 3, 5], d => console.log(d)));
 
 document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
+<h1>Hello Lodash!</h1>
 <div>
   We use Parcel to bundle this sandbox, you can find more info about Parcel
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
